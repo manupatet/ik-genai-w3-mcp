56d4531833c61e7dfd0e9b58807191ec78a2433b
Manu <manupatet@gmail.com>
2025-11-30 01:38:24 +0530

    Delete get_stock_history function
    Removed get_stock_history function for stock price retrieval.

--- mcp_server.py
+++ mcp_server.py
@@ -18,30 +18,6 @@ finnhub_client = finnhub.Client(api_key=FINNHUB_API_KEY)
 mcp = FastMCP("finnhub-MCP-server")
 
 @mcp.tool()
-def get_stock_history(ticker: str) -> dict:
-    """
-    Gets last 7 days of historical price for a given stock ticker.
-    API: stock = yf.Ticker(ticker)
-    """
-    try:
-        stock = yf.Ticker(ticker)
-
-        history = stock.history(period="7d")
-
-        if history.empty:
-            return {"error": f"no history found for this ticker {ticker}"}
-
-        history.reset_index(inplace=True)
-        dates = history['Date'].dt.strftime('%Y-%m-%d').tolist()
-        prices = history['Close'].tolist()
-
-        return {"dates": dates, "prices": prices}
-    
-    except Exception as e:
-        return {"error": str(e)}
-        
-# --- THESE TOOLS STILL USE FINNHUB ---
-@mcp.tool()
 def get_latest_quote(ticker: str) -> dict:
     """
     Fetches the latest quote for a given stock ticker.
